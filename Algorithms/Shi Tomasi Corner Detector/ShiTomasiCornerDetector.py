fd1573cf9d78ba693254a7625a8acadb328b92cf
Ankita Das <ankidas44@gmail.com>
2018-02-20 23:14:33 +0530

    Create ShiTomasiCornerDetector.py

---
+++ Algorithms/Shi Tomasi Corner Detector/ShiTomasiCornerDetector.py
@@ -0,0 +1,26 @@
+#ShiTomasiCornerDetector  --Shows the N strongest corners
+
+#Main Function:cv2.goodFeaturesToTrack(image,no_of_corners,quality_level,min_euclidean_dist_bet_detected_corners)
+
+#image:grayscale image 
+#no_of_corners:Specifies the no. of corners you want ot find
+#quality_level:specifies a minimum quality level of corner,below which every other is rejected.
+#min_euclidean_dist betwn 2 corners
+
+#Program:
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img=cv2.imread('sec.jpg')
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+corners=cv2.goodFeaturesToTrack(gray,25,0.01,10)
+corners=np.int0(corners)
+
+for i in corners:
+    x,y=i.ravel()
+    cv2.circle(img,(x,y),3,255,-1)
+
+plt.imshow(img),plt.show()
+ 
